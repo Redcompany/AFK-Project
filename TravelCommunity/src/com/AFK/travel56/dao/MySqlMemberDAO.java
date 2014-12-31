c5b00942bad85b436ad67967f38a9ff41ccc9163
Redcompany <stereo789@naver.com>
2014-12-31 15:16:37 +0900

    수정한것들

--- TravelCommunity/src/com/AFK/travel56/dao/MySqlMemberDAO.java
+++ TravelCommunity/src/com/AFK/travel56/dao/MySqlMemberDAO.java
@@ -210,13 +210,14 @@ public class MySqlMemberDAO implements MemberDAO {
 			conn = DriverManager.getConnection(databaseURL, username, password);
 			stmt = conn.createStatement();
 
-			String sqlStr = "UPDATE member set member_id='" + memberID + "' , member_password='" + memberPW
-					+ "' , member_gender=" + memberGender + ", member_email= '"
-					+ memberEmail + "' , member_name='" + memberName
-					+ "', member_birth= '" + sdFormat.format(birthDate)
-					+ "', member_phone= '" + memberPhone
-					+ "' where + member_nickname= '" + memberNickName + "';";
-			
+			String sqlStr = "UPDATE member set member_id='" + memberID
+					+ "' , member_password='" + memberPW + "' , member_gender="
+					+ memberGender + ", member_email= '" + memberEmail
+					+ "' , member_name='" + memberName + "', member_birth= '"
+					+ sdFormat.format(birthDate) + "', member_phone= '"
+					+ memberPhone + "' where + member_nickname= '"
+					+ memberNickName + "';";
+
 			stateCheck = stmt.executeUpdate(sqlStr);
 		} catch (ParseException ex3) {
 			Logger.getLogger(MySqlMemberDAO.class.getName()).log(Level.SEVERE,
@@ -347,4 +348,104 @@ public class MySqlMemberDAO implements MemberDAO {
 		}
 		return memberVO;
 	} // end memberLogin()
+
+	public MemberVO findMemberByNameBirthNickname(String memberName,
+			String memberBirth, String memberNickname) {
+		Connection conn = null;
+		Statement stmt = null;
+		MemberVO memberVO = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(databaseURL, username, password);
+			stmt = conn.createStatement();
+			String sqlStr = "select * from member where member_name= '"
+					+ memberName + "' and member_birth= '" + memberBirth
+					+ "' and member_nickname= '" + memberNickname + "'";
+			ResultSet rset = stmt.executeQuery(sqlStr);
+			while (rset.next()) { // list all the authors
+				int membersNumber = rset.getInt("member_number");
+				String membersID = rset.getString("member_id");
+				String membersPW = rset.getString("member_password");
+				boolean membersGender = rset.getBoolean("member_gender");
+				String membersEmail = rset.getString("member_email");
+				String membersName = rset.getString("member_name");
+				String membersPhone = rset.getString("member_phone");
+				String membersBirth = rset.getString("member_birth");
+				String membersNickName = rset.getString("member_nickname");
+
+				memberVO = new MemberVO(membersNumber, membersID, membersPW,
+						membersGender, membersEmail, membersName, membersPhone,
+						membersBirth, membersNickName);
+			}
+
+		} catch (SQLException ex) {
+			Logger.getLogger(MySqlMemberDAO.class.getName()).log(Level.SEVERE,
+					null, ex);
+		} catch (ClassNotFoundException ex2) {
+			Logger.getLogger(MySqlMemberDAO.class.getName()).log(Level.SEVERE,
+					null, ex2);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException ex) {
+				Logger.getLogger(MySqlMemberDAO.class.getName()).log(
+						Level.SEVERE, null, ex);
+			}
+		}
+		return memberVO;
+	}
+
+	public MemberVO findMemberByIDNameBirth(String memberID, String memberName,
+			String memberBirth) {
+		Connection conn = null;
+		Statement stmt = null;
+		MemberVO memberVO = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(databaseURL, username, password);
+			stmt = conn.createStatement();
+			String sqlStr = "select * from member where member_name= '"
+					+ memberName + "' and member_birth= '" + memberBirth
+					+ "' and member_id= '" + memberID + "'";
+			ResultSet rset = stmt.executeQuery(sqlStr);
+			while (rset.next()) { // list all the authors
+				int membersNumber = rset.getInt("member_number");
+				String membersID = rset.getString("member_id");
+				String membersPW = rset.getString("member_password");
+				boolean membersGender = rset.getBoolean("member_gender");
+				String membersEmail = rset.getString("member_email");
+				String membersName = rset.getString("member_name");
+				String membersPhone = rset.getString("member_phone");
+				String membersBirth = rset.getString("member_birth");
+				String membersNickName = rset.getString("member_nickname");
+
+				memberVO = new MemberVO(membersNumber, membersID, membersPW,
+						membersGender, membersEmail, membersName, membersPhone,
+						membersBirth, membersNickName);
+			}
+
+		} catch (SQLException ex) {
+			Logger.getLogger(MySqlMemberDAO.class.getName()).log(Level.SEVERE,
+					null, ex);
+		} catch (ClassNotFoundException ex2) {
+			Logger.getLogger(MySqlMemberDAO.class.getName()).log(Level.SEVERE,
+					null, ex2);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException ex) {
+				Logger.getLogger(MySqlMemberDAO.class.getName()).log(
+						Level.SEVERE, null, ex);
+			}
+		}
+		return memberVO;
+	}
 }
