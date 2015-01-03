2bdc5373444f35c51a3cf79fe6d7eab4361859fb
Redcompany <stereo789@naver.com>
2015-01-03 13:17:16 +0900

    Article 변경

--- TravelCommunity/src/com/AFK/travel56/control/ContinentArticleCommand.java
+++ TravelCommunity/src/com/AFK/travel56/control/ContinentArticleCommand.java
@@ -2,6 +2,7 @@ package com.AFK.travel56.control;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.AFK.travel56.service.ArticleService;
 
@@ -11,8 +12,9 @@ public class ContinentArticleCommand implements Command {
 			throws javax.servlet.ServletException, java.io.IOException {
 		CommandResult commandResult = new CommandResult(
 				"/WEB-INF/view/continentArticle.jsp");
+		HttpSession session =request.getSession(true);
 			ArticleService articleService = new ArticleService();
-			request.setAttribute("continentArticle",
+			session.setAttribute("continentArticle",
 					articleService.showAllArticleByContinent(request.getParameter("continent")));
 		
 		return commandResult;
